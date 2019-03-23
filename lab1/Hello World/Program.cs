7d6cb13159cdeb618b8674d60b2a2c59d7f35d58
chablos <44267943+chablos@users.noreply.github.com>
2019-03-23 15:09:27 -0400

    Add files via upload

---
+++ lab1/Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            // Keep the console window open in debug mode.
+            Console.WriteLine("[Roman Chaban]");
+            Console.ReadKey();
+        }
+    }
+}
